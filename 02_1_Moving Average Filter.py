c2fa72e9bee0ba4b8adfe6da3f04fb1156fa1361
Toma <92250663+jangsejong@users.noreply.github.com>
2022-06-21 09:50:45 +0700

    Create 02_1_Moving Average Filter.py

---
+++ 02_1_Moving Average Filter.py
@@ -0,0 +1,22 @@
+##-- Moving Average Filter --##
+
+class MovingAverageFilter:
+    
+    def __init__(self, MAF_level=4):
+        self.MAF_level = MAF_level
+        self.data = [0 for i in range(MAF_level)]
+        print(self.data)
+
+    def start(self, newData):
+        self.data.pop()
+        self.data.insert(0, newData)
+        return (sum(self.data)/len(self.data))
+
+
+if __name__ == '__main__':
+
+    MovingAverageFilter = MovingAverageFilter(8)
+
+    x = [0, 0, 0, 0, 10, 20, 30, 35, 20, 10, 5, 0, 0, 0, 0]
+    for i in x:
+        print(MovingAverageFilter.start(i))
